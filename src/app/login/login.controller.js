915d5fe38026c6df81c6adb7da76880febadb660
Gasper1one <wixproyecto2@gmail..com>
2023-05-12 11:14:53 -0600

    canviando el origin

--- src/app/login/login.controller.js
+++ src/app/login/login.controller.js
@@ -6,6 +6,7 @@ export default async function LoginController(email, password, router) {
       mode: "cors",
        headers: {
         'Content-Type': 'application/json',
+        Origin:'https://cib-web.vercel.app',
       },
       credentials: "include",
     
