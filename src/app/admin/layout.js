e82bf9a7b9742df51f403eb25c4ddf71fcf167a8
Gasper1one <wixproyecto2@gmail..com>
2023-05-21 00:07:31 -0600

    reparado css

--- src/app/admin/layout.js
+++ src/app/admin/layout.js
@@ -8,7 +8,13 @@ export default function AdminLayout({ children }) {
   <>
  <UserContextProvider>
  <Header/>
+ <div className="sm:ml-64 sm:py-4 sm:px-4">
+        <div className="rounded-lg dark:border-gray-700 mt-14">
+     
    {children}
+   </div>
+     </div>
+      
  </UserContextProvider >
   </>
          
--- src/app/admin/salidas/page.js
+++ src/app/admin/salidas/page.js
@@ -2,12 +2,8 @@
 import Form from "./form.component"
 export default function Page(){
     return(
-        <div className="p-4 sm:ml-64">
-        <div className="p-4  rounded-lg dark:border-gray-700 mt-14">
-     
+       
         <Form/>
-        </div>
-     </div>
-      
+     
     )
 }
