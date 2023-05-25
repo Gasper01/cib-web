3650963ad82a61d3cdb13429d123f182af72c60e
Gasper1one <wixproyecto2@gmail..com>
2023-05-25 11:39:19 -0600

    middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,35 +1,27 @@
 import { NextResponse } from 'next/server';
- import VerifyUser from './app/lib/verifyUser';
+import VerifyUser from './app/lib/verifyUser';
 export async function middleware(req) {
-
- const cookie = req.cookies.get('token')?.value;
- 
+  const cookie = req.cookies.get('token')?.value;
 
   if (req.nextUrl.pathname.includes('/admin')) {
-   if(cookie === undefined){
-    return NextResponse.redirect(new URL('/login', req.url));
- 
-   }
+    if (cookie === undefined) {
+      return NextResponse.redirect(new URL('/login', req.url));
+    }
     const user = await VerifyUser(cookie);
-   if(user.message === 'Anauthorized'){
-    return NextResponse.redirect(new URL('/login', req.url));
-  }
+    if (user.message === 'Anauthorized') {
+      return NextResponse.redirect(new URL('/login', req.url));
+    }
 
     return NextResponse.next();
-   
-   }
+  }
 
-   if (req.nextUrl.pathname.includes('/login')) {
+  if (req.nextUrl.pathname.includes('/login')) {
     const user = await VerifyUser(cookie);
-    if(user.message === 'Anauthorized'){}
-      else{ return NextResponse.redirect(new URL('/admin', req.url));} 
-    
-      return NextResponse.next();
-    } 
-
-    
-    
+    if (user.message === 'Anauthorized') {
+    } else {
+      return NextResponse.redirect(new URL('/admin', req.url));
     }
-  
-  
-  
+
+    return NextResponse.next();
+  }
+}
