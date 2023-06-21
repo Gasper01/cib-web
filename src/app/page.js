67e7ff462bac373ac71d8848503985eeecefd53a
Gasper1one <wixproyecto2@gmail..com>
2023-06-21 13:43:52 -0600

    dele productos

--- src/app/page.js
+++ src/app/page.js
@@ -16,7 +16,6 @@ export default function Page() {
         <h2 className="py-8 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
           Lista de Productos
         </h2>
-        <ProductListHome />
       </main>
       <Footer />
     </ProductsContextProvider>
