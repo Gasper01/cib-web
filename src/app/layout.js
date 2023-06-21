f3dd9822f34043d9b3eb5d6a67d07e288d92f232
Gasper1one <wixproyecto2@gmail..com>
2023-06-21 12:50:14 -0600

    restori context productos provider

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,7 +17,9 @@ export default function RootLayout({ children }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>CIB</title>
       </head>
-      <body className="bg-white dark:bg-slate-800">{children}</body>
+      <body className="bg-white dark:bg-slate-800">
+        <ProductsContextProvider>{children}</ProductsContextProvider>
+      </body>
     </html>
   );
 }
