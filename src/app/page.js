eb65da856a715906ea8791715a552bc87e3737a8
Gasper1one <wixproyecto2@gmail..com>
2023-06-21 13:29:45 -0600

    delete provider

--- src/app/page.js
+++ src/app/page.js
@@ -8,18 +8,14 @@ export default function Page() {
   return (
     <>
       <HeadersHome />
-      <ProductsContextProvider>
-        <main className="max-w-2xl px-4 py-2 mx-auto sm:px-6 sm:py-4 lg:max-w-7xl lg:px-8">
-          <div className="flex">
-            <Allcategory />
-            <SearchProductsHome />
-          </div>
-          <h2 className="py-8 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            Lista de Productos
-          </h2>
-          <ProductListHome />
-        </main>
-      </ProductsContextProvider>
+
+      <main className="max-w-2xl px-4 py-2 mx-auto sm:px-6 sm:py-4 lg:max-w-7xl lg:px-8">
+        <div className="flex"></div>
+        <h2 className="py-8 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          Lista de Productos
+        </h2>
+      </main>
+      <Footer />
     </>
   );
 }
