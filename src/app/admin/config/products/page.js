c4ca49d60a2187b450efd0c24697979d17fc4a7e
Gasper1one <wixproyecto2@gmail..com>
2023-07-05 16:15:14 -0600

    data lore div delete

--- src/app/admin/config/products/page.js
+++ src/app/admin/config/products/page.js
@@ -32,7 +32,6 @@ export default function page() {
           href="/admin/config/products/nuevo"
           className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
         >
-          <div role="status" className="flex items-center justify-center"></div>
           Agregar Producto
         </Link>
       </div>
