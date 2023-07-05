9ed70beaa9b1342ba017867ebccafe461ea3d00d
Gasper1one <wixproyecto2@gmail..com>
2023-07-05 16:10:22 -0600

    update

--- src/app/admin/config/component/form.js
+++ src/app/admin/config/component/form.js
@@ -7,7 +7,6 @@ export default function Form({ isloadin, bottontext, onSubmit, children }) {
         type="submit"
         className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
       >
-        <div role="status" className="flex items-center justify-center"></div>
         {isloadin ? "Creando Producto" : bottontext}
       </button>
     </form>
