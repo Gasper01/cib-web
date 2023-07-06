12ab6e361b4d7055a9edd527c00af4b71727bcd8
Gasper1one <wixproyecto2@gmail..com>
2023-07-06 14:30:46 -0600

    add al category

--- src/app/admin/config/Productos/page.js
+++ src/app/admin/config/Productos/page.js
@@ -1,6 +1,5 @@
-import ProductsTable from "../component/productsTable";
 import Allcategory from "@/components/Allcategorys";
 
 export default function Page() {
-  return <ProductsTable />;
+  return <Allcategory />;
 }
