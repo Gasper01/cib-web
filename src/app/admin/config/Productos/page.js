cce9ffc2d673887f63ee50c748c7ca6ae673f89d
Gasper1one <wixproyecto2@gmail..com>
2023-07-06 14:22:07 -0600

    delete category

--- src/app/admin/config/Productos/page.js
+++ src/app/admin/config/Productos/page.js
@@ -2,13 +2,5 @@ import ProductsTable from "../component/productsTable";
 import Allcategory from "@/components/Allcategorys";
 
 export default function Page() {
-  return (
-    <>
-      <div className="grid grid-cols-2 gap-6 mb-8 xl:grid-cols-4 ">
-        <Allcategory />
-      </div>
-
-      <ProductsTable />
-    </>
-  );
+  return <ProductsTable />;
 }
