d0413540ef6012c2538b5fd2db89758a1dcefa01
Gasper1one <wixproyecto2@gmail..com>
2023-07-06 14:50:36 -0600

    dele product table

--- src/app/admin/config/Productos/page.js
+++ src/app/admin/config/Productos/page.js
@@ -1,15 +1,11 @@
 import Allcategory from "@/components/Allcategorys";
 import SearchProductsHome from "@/components/SaerchProductHome";
-import ProductsTable from "../component/productsTable";
 
 export default function Page() {
   return (
-    <>
-      <div className="flex">
-        <Allcategory />
-        <SearchProductsHome />
-      </div>
-      <ProductsTable />
-    </>
+    <div className="flex">
+      <Allcategory />
+      <SearchProductsHome />
+    </div>
   );
 }
