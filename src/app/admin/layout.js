81ccc7b0cd8c70b740fda0bae63d2537aa5f55a5
Gasper1one <wixproyecto2@gmail..com>
2023-07-06 10:14:00 -0600

    change location prodoct provider

--- src/app/admin/layout.js
+++ src/app/admin/layout.js
@@ -1,15 +1,18 @@
 import UserContextProvider from "@/context/User";
 import AdminHeader from "@/components/AdminHeader";
+import ProductsContextProvider from "@/context/Products";
 export default function AdminLayout({ children }) {
   return (
     <div className="h-screen dark:bg-slate-800">
       <UserContextProvider>
-        <AdminHeader />
-        <div className="px-2 py-2 md:ml-64 md:py-4 md:px-4 ">
-          <div className="rounded-lg dark:border-gray-700 mt-14">
-            {children}
+        <ProductsContextProvider>
+          <AdminHeader />
+          <div className="px-2 py-2 md:ml-64 md:py-4 md:px-4 ">
+            <div className="rounded-lg dark:border-gray-700 mt-14">
+              {children}
+            </div>
           </div>
-        </div>
+        </ProductsContextProvider>
       </UserContextProvider>
     </div>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -6,22 +6,20 @@ import SearchProductsHome from "@/components/SaerchProductHome";
 import ProductsContextProvider from "@/context/Products";
 export default function Home() {
   return (
-    <>
+    <ProductsContextProvider>
       <HeadersHome />
       <main className="max-w-2xl px-4 py-2 mx-auto sm:px-6 sm:py-4 lg:max-w-7xl lg:px-8">
-        <ProductsContextProvider>
-          <div className="flex">
-            <Allcategory />
-            <SearchProductsHome />
-          </div>
-          <h2 className="py-8 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            Lista de Productos
-          </h2>
-          <ProductListHome />
-        </ProductsContextProvider>
+        <div className="flex">
+          <Allcategory />
+          <SearchProductsHome />
+        </div>
+        <h2 className="py-8 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          Lista de Productos
+        </h2>
+        <ProductListHome />
       </main>
 
       <Footer />
-    </>
+    </ProductsContextProvider>
   );
 }
