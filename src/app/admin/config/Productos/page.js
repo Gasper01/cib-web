7ed02a971fe1c2c62f220862b4232e2cd4f17095
Gasper1one <wixproyecto2@gmail..com>
2023-07-06 15:20:47 -0600

    add producta table

--- src/app/admin/config/Productos/page.js
+++ src/app/admin/config/Productos/page.js
@@ -1,15 +1,5 @@
-import Allcategory from "@/components/Allcategorys";
-import SearchProductsHome from "@/components/SaerchProductHome";
 import ProductsTable from "../component/productsTable";
 
 export default function Page() {
-  return (
-    <>
-      <div className="flex">
-        <Allcategory />
-        <SearchProductsHome />
-      </div>
-      <ProductsTable />
-    </>
-  );
+  return <ProductsTable />;
 }
