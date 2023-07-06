64a8b741dbac87a098f518b63ca40cbc48fabaf8
Gasper1one <wixproyecto2@gmail..com>
2023-07-06 15:05:33 -0600

    add category and products

--- src/app/admin/config/Productos/page.js
+++ src/app/admin/config/Productos/page.js
@@ -1,9 +1,15 @@
+import Allcategory from "@/components/Allcategorys";
 import SearchProductsHome from "@/components/SaerchProductHome";
+import ProductsTable from "../component/productsTable";
 
 export default function Page() {
   return (
-    <div className="flex">
-      <SearchProductsHome />
-    </div>
+    <>
+      <div className="flex">
+        <Allcategory />
+        <SearchProductsHome />
+      </div>
+      <ProductsTable />
+    </>
   );
 }
