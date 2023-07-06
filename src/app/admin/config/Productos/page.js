d095c77304a6b299e80226173b068130da401dcf
Gasper1one <wixproyecto2@gmail..com>
2023-07-06 14:54:16 -0600

    dele category

--- src/app/admin/config/Productos/page.js
+++ src/app/admin/config/Productos/page.js
@@ -1,10 +1,8 @@
-import Allcategory from "@/components/Allcategorys";
 import SearchProductsHome from "@/components/SaerchProductHome";
 
 export default function Page() {
   return (
     <div className="flex">
-      <Allcategory />
       <SearchProductsHome />
     </div>
   );
