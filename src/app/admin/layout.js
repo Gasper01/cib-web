e7e2e5a1e14bb18f96ee3e95f88981f7bca1ed1b
Gasper1one <wixproyecto2@gmail..com>
2023-07-06 09:51:44 -0600

    move product provider

--- src/app/admin/layout.js
+++ src/app/admin/layout.js
@@ -4,16 +4,16 @@ import ProductsContextProvider from "@/context/Products";
 export default function AdminLayout({ children }) {
   return (
     <UserContextProvider>
-      <div className="h-screen dark:bg-slate-800">
-        <AdminHeader />
-        <ProductsContextProvider>
+      <ProductsContextProvider>
+        <div className="h-screen dark:bg-slate-800">
+          <AdminHeader />
           <div className="px-2 py-2 md:ml-64 md:py-4 md:px-4 ">
             <div className="rounded-lg dark:border-gray-700 mt-14">
               {children}
             </div>
           </div>
-        </ProductsContextProvider>
-      </div>
+        </div>
+      </ProductsContextProvider>
     </UserContextProvider>
   );
 }
