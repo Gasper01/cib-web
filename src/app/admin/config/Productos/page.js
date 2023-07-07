5112b453288e87b03e2d51ea422fad98da4b20b1
Gasper1one <wixproyecto2@gmail..com>
2023-07-07 11:54:58 -0600

    dele categorys

--- src/app/admin/config/Productos/page.js
+++ src/app/admin/config/Productos/page.js
@@ -1,9 +1,3 @@
-import AllCategorys from "@/components/Categorys";
-
 export default function Page() {
-  return (
-    <div className="flex">
-      <AllCategorys />
-    </div>
-  );
+  return <div className="flex"></div>;
 }
