68e028296a42153c60ed933195266361bee40dd6
Gasper1one <wixproyecto2@gmail..com>
2023-07-07 10:24:11 -0600

    #23

--- src/components/Allcategorys.js
+++ src/components/Allcategorys.js
@@ -1,16 +1,15 @@
 "use client";
-import { ProductsData } from "@/context/ProductContext";
-import { useState } from "react";
+//import { ProductsData } from "@/context/ProductContext";
+//import { useState } from "react";
 
 export default function Allcategory() {
-  const [isOpen, setOpen] = useState(false);
-  const { selectedCategory, setSelectedCategory, uniqueCategories } =
-    ProductsData();
+  // const [isOpen, setOpen] = useState(false);
+  // const { selectedCategory, setSelectedCategory, uniqueCategories } =
+  //  ProductsData();
 
-  const onClickSetCategory = (category) => {
-    setSelectedCategory(category);
-    setOpen(!isOpen);
-  };
-  console.log(uniqueCategories);
+  // const onClickSetCategory = (category) => {
+  //  setSelectedCategory(category);
+  // setOpen(!isOpen);
+  // };
   return <p>hola</p>;
 }
