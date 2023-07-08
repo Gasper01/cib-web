1a59ad9a4d6dde4d7850200054eb20bd2584a088
Gasper1one <wixproyecto2@gmail..com>
2023-07-08 15:05:16 -0600

    add nueva categorys

--- src/app/admin/config/Productos/page.js
+++ src/app/admin/config/Productos/page.js
@@ -1,9 +1,15 @@
-import Categorys from "@/components/Categorys";
+import Categorys from "../components/Categorys";
+import ProductsTable from "../component/productsTable";
+import SearchProductsHome from "@/components/SaerchProductHome";
 
 export default function Page() {
   return (
-    <div className="flex">
-      <Categorys />
-    </div>
+    <>
+      <div className="flex">
+        <Categorys />
+        <SearchProductsHome />
+      </div>
+      <ProductsTable />
+    </>
   );
 }
