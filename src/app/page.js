205401996a48df80aebc4a8e7fbb2259944ebe63
Gasper1one <wixproyecto2@gmail..com>
2023-07-08 14:32:39 -0600

    dele category

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,3 @@
-import Categorys from "@/components/Categorys";
 import Footer from "@/components/Footer";
 import HeadersHome from "@/components/HeadersHome";
 import ProductListHome from "@/components/ProductListHome";
@@ -9,7 +8,6 @@ export default function Home() {
       <HeadersHome />
       <main className="max-w-2xl px-4 py-2 mx-auto sm:px-6 sm:py-4 lg:max-w-7xl lg:px-8">
         <div className="flex">
-          <Categorys />
           <SearchProductsHome />
         </div>
         <h2 className="py-8 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
