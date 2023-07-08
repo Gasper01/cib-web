19a57e4aef6b375f11ab2ab2ef871e10aa444885
Gasper1one <wixproyecto2@gmail..com>
2023-07-08 14:59:48 -0600

    solo category

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,8 @@
 import Categorys from "@/components/Categorys";
-import Footer from "@/components/Footer";
-import HeadersHome from "@/components/HeadersHome";
-import ProductListHome from "@/components/ProductListHome";
 export default function Home() {
   return (
-    <>
-      <HeadersHome />
-      <main className="max-w-2xl px-4 py-2 mx-auto sm:px-6 sm:py-4 lg:max-w-7xl lg:px-8">
-        <div className="flex">
-          <Categorys />
-        </div>
-        <h2 className="py-8 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-          Lista de Productos
-        </h2>
-        <ProductListHome />
-      </main>
-      <Footer />
-    </>
+    <div className="flex">
+      <Categorys />
+    </div>
   );
 }
