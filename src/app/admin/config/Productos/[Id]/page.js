3e091793f49bcae870fb9d104f21768941b2e275
Gasper1one <wixproyecto2@gmail..com>
2023-07-08 15:47:28 -0600

    dele

--- src/app/admin/config/Productos/[Id]/page.js
+++ src/app/admin/config/Productos/[Id]/page.js
@@ -1,72 +1,7 @@
 "use client";
 
-import Cards from "@/app/admin/config/component/Cards";
-import Form from "@/app/admin/config/component/Form";
-import Input from "@/app/admin/config/component/Input";
-import { AddProducts } from "@/app/admin/config/hook/addProduct";
-
 export default function Page({ params }) {
   const { Id } = params;
-
-  let Title = "Agregar Nuevo Producto";
-  let bottontext = "Agregar";
-
-  if (Id !== "nuevo") {
-    Title = "Editar Producto";
-    bottontext = "Salvar Cambios";
-  }
-  const { onSubmitForm, handleChange, formData, isloadin, message } =
-    AddProducts(Id);
-
-  return (
-    <Cards
-      message={message.message}
-      title={Title}
-      url={"/admin/config/Productos"}
-      texturl={"Regresar a Productos"}
-    >
-      <Form isloadin={isloadin} bottontext={bottontext} onSubmit={onSubmitForm}>
-        <Input
-          label={"nombre"}
-          type={"text"}
-          name={"nombre"}
-          placeholder={'Tubos de 4"'}
-          value={formData.nombre}
-          onChange={handleChange}
-        />
-        <Input
-          label={"Codigo"}
-          type={"text"}
-          name={"codigo"}
-          placeholder={""}
-          value={formData.codigo}
-          onChange={handleChange}
-        />
-        <Input
-          label={"CantIdad"}
-          type={"number"}
-          name={"cantIdad"}
-          placeholder={""}
-          value={formData.cantIdad}
-          onChange={handleChange}
-        />
-        <Input
-          label={"UND"}
-          type={"text"}
-          name={"unIdad"}
-          placeholder={"UND, GALONES, LITROS, CUARTOS"}
-          value={formData.unIdad}
-          onChange={handleChange}
-        />
-        <Input
-          label={"Imagen"}
-          type={"text"}
-          name={"ImgUrl"}
-          placeholder={"www.pictures/tubos.jpg"}
-          value={formData.ImgUrl}
-          onChange={handleChange}
-        />
-      </Form>
-    </Cards>
-  );
+  console.log(Id);
+  return <p>hola</p>;
 }
