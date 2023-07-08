618dace3090ad7ac1105e43a279bda5dbdf4b259
Gasper1one <wixproyecto2@gmail..com>
2023-07-08 16:25:22 -0600

    use client

--- src/app/admin/config/Productos/[Id]/page.js
+++ src/app/admin/config/Productos/[Id]/page.js
@@ -1,4 +1,72 @@
+"use client";
+
+import Cards from "../../components/Cards";
+import Form from "../../components/Form";
+import Input from "../../components/Input";
+import { AddProducts } from "../../hook/addProduct";
+
 export default function Page({ params }) {
   const { Id } = params;
-  return <p>hola</p>;
+
+  let Title = "Agregar Nuevo Producto";
+  let bottontext = "Agregar";
+
+  if (Id !== "nuevo") {
+    Title = "Editar Producto";
+    bottontext = "Salvar Cambios";
+  }
+  const { onSubmitForm, handleChange, formData, isloadin, message } =
+    AddProducts(Id);
+
+  return (
+    <Cards
+      message={message.message}
+      title={Title}
+      url={"/admin/config/products"}
+      texturl={"Regresar a Productos"}
+    >
+      <Form isloadin={isloadin} bottontext={bottontext} onSubmit={onSubmitForm}>
+        <Input
+          label={"nombre"}
+          type={"text"}
+          name={"nombre"}
+          placeholder={'Tubos de 4"'}
+          value={formData.nombre}
+          onChange={handleChange}
+        />
+        <Input
+          label={"Codigo"}
+          type={"text"}
+          name={"codigo"}
+          placeholder={""}
+          value={formData.codigo}
+          onChange={handleChange}
+        />
+        <Input
+          label={"Cantidad"}
+          type={"number"}
+          name={"cantidad"}
+          placeholder={""}
+          value={formData.cantidad}
+          onChange={handleChange}
+        />
+        <Input
+          label={"UND"}
+          type={"text"}
+          name={"unIdad"}
+          placeholder={"UND, GALONES, LITROS, CUARTOS"}
+          value={formData.unIdad}
+          onChange={handleChange}
+        />
+        <Input
+          label={"Imagen"}
+          type={"text"}
+          name={"ImgUrl"}
+          placeholder={"www.pictures/tubos.jpg"}
+          value={formData.ImgUrl}
+          onChange={handleChange}
+        />
+      </Form>
+    </Cards>
+  );
 }
