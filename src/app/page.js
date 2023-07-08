37234850332bea481014f26b6782357977993cbc
Gasper1one <wixproyecto2@gmail..com>
2023-07-08 14:39:45 -0600

    dele sar products

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,12 @@
 import Footer from "@/components/Footer";
 import HeadersHome from "@/components/HeadersHome";
 import ProductListHome from "@/components/ProductListHome";
-import SearchProductsHome from "@/components/SaerchProductHome";
 export default function Home() {
   return (
     <>
       <HeadersHome />
       <main className="max-w-2xl px-4 py-2 mx-auto sm:px-6 sm:py-4 lg:max-w-7xl lg:px-8">
-        <div className="flex">
-          <SearchProductsHome />
-        </div>
+        <div className="flex"></div>
         <h2 className="py-8 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
           Lista de Productos
         </h2>
