2f760f1fb11d2231b1971f0a879791103eba52c1
Gasper1one <wixproyecto2@gmail..com>
2023-07-08 14:48:22 -0600

    delete sear productos

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,6 @@ import Categorys from "@/components/Categorys";
 import Footer from "@/components/Footer";
 import HeadersHome from "@/components/HeadersHome";
 import ProductListHome from "@/components/ProductListHome";
-import SearchProductsHome from "@/components/SaerchProductHome";
 export default function Home() {
   return (
     <>
@@ -10,7 +9,6 @@ export default function Home() {
       <main className="max-w-2xl px-4 py-2 mx-auto sm:px-6 sm:py-4 lg:max-w-7xl lg:px-8">
         <div className="flex">
           <Categorys />
-          <SearchProductsHome />
         </div>
         <h2 className="py-8 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
           Lista de Productos
