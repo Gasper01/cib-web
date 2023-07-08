e779bbebbb8a57fc5995b2503e668d7271877cec
Gasper1one <wixproyecto2@gmail..com>
2023-07-08 14:08:42 -0600

    sear productos

--- src/app/admin/config/Productos/page.js
+++ src/app/admin/config/Productos/page.js
@@ -1,5 +1,6 @@
 import Categorys from "@/components/Categorys";
 import ProductsTable from "../component/productsTable";
+import SearchProductsHome from "@/components/SaerchProductHome";
 
 export default function Page() {
   return (
